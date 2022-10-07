32f64f880f65e11e776cd17e184aa7b6b2bfdf3a
alanjv3c <alanflores12@hotmail.com>
2022-10-06 23:33:29 -0600

    fix dependencias

--- src/main/java/com/tienda/backend/BackendApplication.java
+++ src/main/java/com/tienda/backend/BackendApplication.java
@@ -3,8 +3,12 @@ package com.tienda.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableJpaRepositories(basePackages = { "pl.springBootStarter.app" })
+@ComponentScan("modelos")
 public class BackendApplication {
 
 	public static void main(String[] args) {
